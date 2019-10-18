9b42cd3f451ece594fbc6b7dacd0b39ff28b9cbf
DeAndre Johnson <dre@phreshr.com>
2019-10-19 01:42:11 +0800

    Stretch Progress 💪 Fetch data with graphql

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,37 +1,54 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { useQuery } from '@apollo/client';
 import CharacterCard from './CharacterCard';
 import SearchForm from './SearchForm';
+import { CHARACTERS_QUERY } from '../grapql/queries';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [characters, setCharacters] = useState([]);
+
+  // Graphql
   const [query, setQuery] = useState('');
 
-  useEffect(() => {
-    async function getCharacters() {
-      const response = await axios.get(
-        'https://rickandmortyapi.com/api/character/',
-      );
-      setCharacters(response.data.results);
-    }
-    getCharacters();
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+  const { loading, error, data } = useQuery(CHARACTERS_QUERY, {
+    variables: { name: query },
+  });
+
+  if (data) {
+    console.log(data.characters.results);
+  }
 
   const handleSearch = e => setQuery(e.target.value);
+
+  // useEffect(() => {
+  //   async function getCharacters() {
+  //     const response = await axios.get(
+  //       'https://rickandmortyapi.com/api/character/',
+  //     );
+  //     setCharacters(response.data.results);
+  //   }
+  //   getCharacters();
+  //   // TODO: Add API Request here - must run in `useEffect`
+  //   //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+  // }, []);
+
+  if (loading) return null;
+  if (error) return `Error! ${error}`;
 
   return (
     <section className='character-list'>
       <h2>Characters</h2>
       <SearchForm handleSearch={handleSearch} query={query} />
-      {characters.map(character => {
+      {data.characters.results.map(character => {
         return character.name.toLowerCase().includes(query.toLowerCase()) ? (
-          <Link to={`/character/${character.id}`} key={character.id}>
-            <CharacterCard character={character} />
-          </Link>
+          <Fragment key={character.id}>
+            <Link to={`/character/${character.id}`}>
+              <CharacterCard character={character} />
+            </Link>
+          </Fragment>
         ) : null;
       })}
     </section>
