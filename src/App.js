c585d9bf84365a2738dc5f7d9af745a2d10ff1d6
DeAndre Johnson <dre@phreshr.com>
2019-10-18 23:25:45 +0800

    add global styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
-import Header from "./components/Header.js";
-
+import React from 'react';
+import Header from './components/Header.js';
+import GlobalStyles from './style/GlobalStyles';
 
 export default function App() {
   return (
-    <main>
-      <Header />
-    </main>
+    <>
+      <GlobalStyles />
+      <main>
+        <Header />
+      </main>
+    </>
   );
 }
