5213bab4a85a360f4c13ac74623099d4e565239c
DeAndre Johnson <dre@phreshr.com>
2019-10-18 23:31:08 +0800

    add routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import Header from './components/Header.js';
+import { Switch, Route } from 'react-router-dom';
 import GlobalStyles from './style/GlobalStyles';
+
+import Header from './components/Header.js';
+import WelcomePage from './components/WelcomePage';
+import CharacterList from './components/CharacterList';
 
 export default function App() {
   return (
@@ -8,6 +12,14 @@ export default function App() {
       <GlobalStyles />
       <main>
         <Header />
+        <Switch>
+          <Route exact path='/'>
+            <WelcomePage />
+          </Route>
+          <Route exact path='/characters'>
+            <CharacterList />
+          </Route>
+        </Switch>
       </main>
     </>
   );
